6a5a9b1e7b9ba29d0eb65c7b1c6eadcabb7d1ddd
VRamazing <r.vignesh149@gmail.com>
2017-07-28 16:54:39 +0530

    UI improved Functionality added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,33 @@
- 
+	var finalstr = ""
+	var update = function(str,oper){
+		switch(oper){
+			case 'num':
+				finalstr += str;
+				break;
+
+			case 'op':
+				if(str == '='){
+					//Parse the final String and perform calculation
+					//Main work comes here
+				}
+				else{
+					finalstr += str;
+				}
+				
+
+
+			case 'sp':
+				if(str == 'AC') finalstr = ''
+
+				if(str == 'CE') finalstr = ''
+
+
+		}
+			
+		
+
+		document.getElementById('disp-str').innerHTML = finalstr;
+
+	} 
 
 	
-	var init = function(){
-		
-	}
-	window.onload = init;
-
-
