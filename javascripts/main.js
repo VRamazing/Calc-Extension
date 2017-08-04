11e8e49d3025315a0d9d995b48eb6909d330db18
VRamazing <r.vignesh149@gmail.com>
2017-08-04 14:04:01 +0530

    Final String parsed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,33 +1,66 @@
-	var finalstr = ""
+	var finalstr = ""; //shows the progress of operations
+	var inpstr = ""; //what is being inputed
 	var update = function(str,oper){
 		switch(oper){
 			case 'num':
+				inpstr = str
+			    display(inpstr);
 				finalstr += str;
 				break;
 
 			case 'op':
-				if(str == '='){
-					//Parse the final String and perform calculation
-					//Main work comes here
+				if( finalstr.endsWith('x') || finalstr.endsWith('/') || finalstr.endsWith('+') || finalstr.endsWith('-')){
+					finalstr  = finalstr.slice(0,finalstr.length-1)
+					
 				}
-				else{
-					finalstr += str;
-				}
-				
+ 				if(str == '='){
+ 					//Parse the final String and perform calculation
 
+					
+ 					var nums = finalstr.split(/[+\-x/]/);
+ 					var operands = [];
+ 					var dummy = finalstr;
+ 					while(/[+\-x/]/.test(dummy)){
+ 						
+ 						operands.push(/[+\-x/]/.exec(dummy)[0]);
+ 						dummy = dummy.substr(dummy.indexOf(operands[operands.length-1]) + 1,dummy.length-1);
+ 					}
+
+ 					console.log(operands);
+
+ 					
+ 					//97 / 3 * 5 + 6 / 9 + 10 - 10
+					//convert into 2 list 
+					//1.Integers and Floats
+					//2. Operands
+
+
+				}
+				finalstr += str;
+				inpstr = str
+				display(inpstr);
+				break;
 
 			case 'sp':
-				if(str == 'AC') finalstr = ''
+				if(str == 'AC') finalstr = '';display(' ')
+				if(str == 'CE' && inpstr !== ''  ){
 
-				if(str == 'CE') finalstr = ''
-
+					finalstr = finalstr.substr(0,finalstr.length - 1);
+					inpstr = '';
+					display(inpstr);
+				} 
+				break;
 
 		}
 			
 		
 
-		document.getElementById('disp-str').innerHTML = finalstr;
+		document.getElementById('disp-str').innerHTML = finalstr; //shows the progress of operations
 
 	} 
+
+	var display = function(str){
+		document.getElementById('output-str').innerHTML = str; //shows whats  being input and output will be displayed here.
+	}
 
 	
