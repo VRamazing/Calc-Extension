e18e8f3f708e98d4e72bb5a98160345d90529eb5
VRamazing <r.vignesh149@gmail.com>
2017-08-04 14:25:01 +0530

    Functionaliy for operations added

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,23 +16,47 @@
  				if(str == '='){
  					//Parse the final String and perform calculation
 
-					
+					//Extracts and stores numbers in num array
  					var nums = finalstr.split(/[+\-x/]/);
  					var operands = [];
  					var dummy = finalstr;
+
+ 					//Extracts and Stores operands in operands array
  					while(/[+\-x/]/.test(dummy)){
  						
  						operands.push(/[+\-x/]/.exec(dummy)[0]);
  						dummy = dummy.substr(dummy.indexOf(operands[operands.length-1]) + 1,dummy.length-1);
  					}
+ 					var result =  parseFloat(nums[0]);
+ 					var j = 0;
+ 					for(var i = 1 ; i < nums.length ; i++){
+ 						switch(operands[j]){
+ 							case 'x':
+ 								result *= parseFloat(nums[i])
+ 								break;
+ 							
+ 							case '/':
+ 								result /= parseFloat(nums[i])
+ 								break;
+ 							
+ 							case '+':
+ 								result += parseFloat(nums[i])
+ 								break;
 
- 					console.log(operands);
+ 							case '-':
+ 								result -= parseFloat(nums[i])
+ 								break;
+ 						}
+ 						j++;
+ 					}
+
+ 					finalstr = result.toString();
+ 					inpstr = finalstr
+					display(inpstr);
+					break;
 
  					
- 					//97 / 3 * 5 + 6 / 9 + 10 - 10
-					//convert into 2 list 
-					//1.Integers and Floats
-					//2. Operands
+
 
 
 				}
